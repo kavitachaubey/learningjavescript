2f7be90fcc485cf0012064af8e4fe9e8582c99de
kavitachaubey <chaubeykavita716@gmail.com>
2021-12-17 22:42:51 +0530

    'div'

--- basic.js
+++ basic.js
@@ -11,4 +11,9 @@ console.log(sub(2,4))
 function mul (num1 , num2){
     return num1*num2;
 }
-console.log(mul(2,4))
+console.log(mul(2,4))       
+
+function  div (num1 , num2){
+    return num1/num2;
+}
+console.log(div(4,2))    
