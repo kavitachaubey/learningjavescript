f813c3910a242ef13d74212ac5c72dce3b5782c8
kavitachaubey <chaubeykavita716@gmail.com>
2021-12-19 22:54:30 +0530

    'indexOf'

---
+++ indexOf.js
@@ -0,0 +1,11 @@
+let game = "We are playing Baseball.";
+game = game.indexOf("Baseball");
+console.log(game)
+
+let food = "We are eating momos.";
+food = food.indexOf("s");
+console.log(food)
+
+let person = "you are good person.";
+person = person.indexOf("z");
+console.log(person)
