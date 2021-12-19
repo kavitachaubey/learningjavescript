6d486d57fba32149e16a26524d9c8f43d228ba9f
kavitachaubey <chaubeykavita716@gmail.com>
2021-12-19 22:39:58 +0530

    trim

--- pattern.js
+++ pattern.js
@@ -1,19 +1,19 @@
-// for(let i=1;i<=10;i++){
-//     for(let j=1;j<=i;j++){
-//         process.stdout.write('*');
-//     }
-//     console.log();
-// }
+for(let i=1;i<=10;i++){
+    for(let j=1;j<=i;j++){
+        process.stdout.write('*');
+    }
+    console.log();
+}
 
-// for(let i=1;i<=10;i++){
-//     for(let k=10;k>=i;k--){
-//         process.stdout.write(' ');
-//     }
-//     for(let j=1;j<=i;j++){
-//         process.stdout.write('*');
-//     }
-//     console.log();
-// }
+for(let i=1;i<=10;i++){
+    for(let k=10;k>=i;k--){
+        process.stdout.write(' ');
+    }
+    for(let j=1;j<=i;j++){
+        process.stdout.write('*');
+    }
+    console.log();
+}
 
 for(let i=1;i<=10;i++){
     for(let k=10;k>=i;k--){
