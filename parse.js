de22341e5288308ddc42c987b56fa22514c79de5
kavitachaubey <chaubeykavita716@gmail.com>
2021-12-22 19:20:54 +0530

    parse

---
+++ parse.js
@@ -0,0 +1,8 @@
+console.log(parseInt("24.6464"));
+console.log(parseInt("24"));
+console.log(parseInt("24hello"));
+console.log(parseInt("heloo 23"));
+
+console.log(parseFloat("24.56775"));
+console.log(parseFloat("hello 3 hi"));
+console.log(parseFloat("24"));
