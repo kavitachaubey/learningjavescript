bc5f22d82109aaa3b677ca0db833d5b926682e68
Kavita <chaubeykavita716@gmail.com>
2022-01-06 23:04:11 +0530

    lexical scope

---
+++ adFunction/lexical-scope.js
@@ -0,0 +1,12 @@
+function outer(){
+    let movie = "spiderman - far from home"
+    function inner(){
+        let movie = 'spiderman - no home';
+        function extraInner(){
+            console.log(movie.toUpperCase());
+        }
+        extraInner();
+    }
+    inner();
+}
+outer();
