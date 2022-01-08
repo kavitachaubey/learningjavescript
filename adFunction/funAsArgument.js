427cc41476b1e759a25d9ae69223c253c7d5a0ff
Kavita <chaubeykavita716@gmail.com>
2022-01-08 22:19:02 +0530

    function as argument

---
+++ adFunction/funAsArgument.js
@@ -0,0 +1,32 @@
+function callThreeTimes(f){
+    f();
+    f();
+    f();
+}
+
+function happy(){
+    console.log("I'm so Happy...");
+}
+
+function sad(){
+    console.log("I'm so sad...")
+}
+// callThreeTimes(happy);
+// callThreeTimes(sad);
+
+
+function withLoop(action,num){
+    for(let i=0;i<num;i++){
+        action();
+    }
+}
+// withLoop(happy,13);
+
+
+function withRandom(f1,f2){
+    let rand = Math.random();
+    console.log(rand);
+    (rand <= 0.5)? f1() : f2();
+}
+withRandom(sad , happy);
+
