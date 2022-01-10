9af3efbad58eca31b2d593238b38988f48ec0362
Kavita <chaubeykavita716@gmail.com>
2022-01-10 20:37:57 +0530

    not important

--- modifying_arr.js
+++
@@ -1,7 +0,0 @@
-let shoppingList = ["potatoes", "raddish", "tomatoes"]
-
-shoppingList[2] = "milk";
-shoppingList[shoppingList.length] = "cabbage";  //shoppinglist.length=3;
-shoppingList[4] = "curd";
-
-console.log(shoppingList);
--- ternary.js
+++
@@ -1,4 +0,0 @@
-let num=10;
-(num===7)? console.log("Yeah it's true") : console.log("Nah it's not");
-
-let color = num === 10 ? console.log("Yellow") :console.log("Red");
