d492db1c18c3743e19d03f6427dec832e523122c
Kavita <chaubeykavita716@gmail.com>
2022-01-10 22:29:50 +0530

    callback

--- app.js
+++ app.js
@@ -1,2 +1,14 @@
-alert("It's working");
-console.log("hello");
+// alert("It's working");
+// console.log("hello");
+
+ const makealert = function(){
+    alert("Let start a real javascript");
+}
+
+// const btn = document.querySelector('button');
+// btn.addEventListener("click",makealert);
+
+const btn = document.querySelector('button');
+btn.addEventListener("click",function(){
+    alert("Welcome!!!")
+})
