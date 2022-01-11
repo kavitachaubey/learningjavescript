07a4b087da4a508f37fe37801c3af24d62bc36a4
Kavita <chaubeykavita716@gmail.com>
2022-01-11 18:15:12 +0530

    arrow function

---
+++ arrayCallback/arrowFunction.js
@@ -0,0 +1,22 @@
+const value = function(v){
+    return v*4;
+}
+console.log(value(4));
+
+
+
+const number = n => {
+    return n*n;
+}
+console.log(number(5));
+
+
+const twice = num => {
+    return num*2;
+}
+console.log(twice(4));
+
+const addition = (x,y) =>{
+    return x+y;
+}
+console.log(addition(5,86))
